f7e8d5fc8e929f5d0c6cc5f14c61da0781e05f99
Matthew Bojey <mbojey@gmail.com>
2013-10-28 10:14:27 -0700

    added belongs_to to keywords

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,4 +2,8 @@ class Keyword < ActiveRecord::Base
 
         validates :keyword, presence: true
         validates :experience_id, :posting_id, :skill_id, presence: true
+
+        belongs_to :skill
+        belongs_to :experience
+        belongs_to :posting
 end
