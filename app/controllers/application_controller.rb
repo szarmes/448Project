d097e644f46b72c72d02b1f1b741aff942f095a5
Duncan Szarmes <szarmes@gmail.com>
2013-10-30 10:39:00 -0700

    Gender working andemployer/employee on the way

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) do |u|
         u.permit :username, :email, :password, :password_confirmation, :fname, :lname, :user_id, 
-        :employee, :employer, :phone_number, :city, :province, 
+        :employee, :employer, :phone_number, :city, :province,  :gender,
         :country, :birthday, :goals, :company_name, :company_address, :field
       end
     end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+#config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 # Initialize the Rails application.
 First_Website::Application.initialize!
 
