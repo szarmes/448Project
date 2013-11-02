60b4a544a962acd25569597e2dc72fdee6c17b7a
Matthew Bojey <mbojey@gmail.com>
2013-11-02 13:07:19 -0700

    View tests for home page green! Yay

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
 
 describe "StaticPages" do
 
+  let(:employee) { create(:user, :employee) }
+  let(:employer) { create(:user, :employer) }
+  
   subject { page }
 
   shared_examples_for "all static pages" do
@@ -23,27 +28,27 @@ describe "StaticPages" do
   describe "should have links that go to expected pages" do
     describe "employer signed in" do
       before do 
-        post_via_redirect new_user_session_path, :email => 'employer@test.ca', :password => 'testtest'
+        login_as(employer, :scope => :user )
       end
       it "should have links to postings when employer signed in" do
        visit root_path
        click_link 'Postings'
-       expect(page).to have_title('Your Postings')
+       expect(page).to have_content('Posting')
       end
     end
 
     describe "employee signed in" do
       before do 
-        post_via_redirect new_user_session_path, :email => 'employee@test.ca', :password => 'testtest'
+        login_as(employee, :scope => :user )
       end
       it "should have links to skills, experiences, references when employee signed in" do
         visit root_path
         click_link "Skills"
-        expect(page).to have_title('Your Skills')
+        expect(page).to have_content('Skill')
         click_link "Experiences"
-        expect(page).to have_title('Your Experiences')
+        expect(page).to have_content('Experience')
         click_link "References"
-        expect(page).to have_title('Your References')
+        expect(page).to have_content('Reference')
       end
     end
   end
