4b3e58ca56740cfeb1d039e5dd964c0f6dcd88a1
Matthew Bojey <mbojey@gmail.com>
2013-11-05 21:35:10 -0800

    Added ability to edit and delete skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -3,9 +3,40 @@ class SkillsController < ApplicationController
   def index
   	@skill = Skill.new
     @user = current_user
+    @userID = current_user.user_id
+    @skills = Skill.where(:user_id => current_user.user_id).paginate(page: params[:page])
   end
 
   def show
+
+  end
+
+  def update
+    @skill = Skill.find(params[:id])
+    if @skill.update_attributes(skill_params)
+      @skill.save
+      flash[:success] = "Changes saved"
+      redirect_to '/skills'         
+    else
+      flash[:error] = "Changes not saved."
+      redirect_to '/skills'                       
+    end
+  end
+
+  def edit
+    @skill = Skill.find(params[:id])
+    @user = current_user
+  end
+
+  def destroy
+    @skill = Skill.find(params[:id])
+    if (user_signed_in? && current_user.user_id = @skill.user_id && !@current_user.employer?)
+      @skill.destroy
+      flash[:success] = "Skill Removed."
+    else
+      flash[:error] = "No access"
+    end
+    redirect_to '/skills'
 
   end
 
@@ -16,18 +47,17 @@ class SkillsController < ApplicationController
   		@skill.user_id = current_user.user_id
   		if(@skill.save)
   			flash[:success] = "Skill created!"
-        	redirect_to root_path
-        else
+      	redirect_to "/skills"
+      else
   			flash[:error] = "Fill in all required fields"
-        	redirect_to root_path
-    end
+       	redirect_to "/skills"
+      end
+      @skills = Skill.where(:user_id => current_user.user_id).paginate(page: params[:page])
+      @userID = current_user.user_id
   	else
   		flash[:error] = "No access"
-        redirect_to root_path
-
-  	end
-
-  	
+      redirect_to root_path
+    end
   end
 
   private
--- spec/controllers/skill_controller_spec.rb
+++ spec/controllers/skill_controller_spec.rb
@@ -4,13 +4,7 @@ describe SkillsController do
 
   let(:employee) { create(:user, :employee) }
   let(:employer) { create(:user, :employer) }
-  
-  describe "GET #index" do
-    it "renders index skill page" do
-      get :index
-      response.should render_template :index
-    end
-  end
+  let(:test)     { create(:skill) }
   describe "user not signed in" do
     it "should not be able to go to create" do
       get 'create'
@@ -24,12 +18,12 @@ describe SkillsController do
       end
       it "should be able to submit a skill through create" do
         post 'create', :skill => { "label" => "title", "desc" => "content", "skill_id" => 0 }
-        response.should redirect_to '/'
+        response.should redirect_to '/skills'
         flash[:success].should_not be_nil
       end
       it "should not have success flash if unable to save" do
         post 'create', :skill => { "label" => "", "desc" => "content" }
-        response.should redirect_to '/'
+        response.should redirect_to '/skills'
         flash[:error].should_not be_nil
       end
     end
@@ -46,6 +40,30 @@ describe SkillsController do
         post 'create', :skill => { "label" => "", "desc" => "content" }
         response.should redirect_to '/'
         flash[:error].should_not be_nil
+      end
+    end
+  end
+  describe "#destroy" do
+    describe "employee signed in" do
+      before do
+        sign_in employee
+        test.save
+      end
+      it "delete a skill through destroy" do
+        post 'destroy', :id => 1
+        response.should redirect_to '/skills'
+        flash[:success].should_not be_nil
+      end
+    end
+    describe "employer signed in" do
+      before do
+        sign_in employer
+        test.save
+      end
+      it "should not be able to delete a skill" do
+        post 'destroy', :id => 1
+        response.should redirect_to '/skills'
+        flash[:error].should == "No access"
       end
     end
   end
