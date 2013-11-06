576facf766afc0d6348efef89a0f496cefac8fad
Matthew Bojey <mbojey@gmail.com>
2013-11-05 22:28:29 -0800

    View all experiences, edit and delete experiences added and tested

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,9 +3,45 @@ class ExperiencesController < ApplicationController
   def index
   	@experience = Experience.new
     @user = current_user
+    @userID = current_user.user_id
+    @experiences = Experience.where(:user_id => current_user.user_id).paginate(page: params[:page])
   end
 
   def show
+
+  end
+
+  def update
+    @experience = Experience.find(params[:id])
+    if (user_signed_in? && current_user.user_id == @experience.user_id && !current_user.employer)
+      if @experience.update_attributes(experience_params)
+        @experience.save
+        flash[:success] = "Changes saved"
+        redirect_to '/experiences'         
+      else
+        flash[:error] = "Changes not saved."
+        redirect_to '/experiences'                       
+      end
+    else
+      flash[:error] = "No access"
+      redirect_to '/experiences'  
+    end
+  end
+
+  def edit
+    @experience = Experience.find(params[:id])
+    @user = current_user
+  end
+
+  def destroy
+    @experience = Experience.find(params[:id])
+    if (user_signed_in? && current_user.user_id == @experience.user_id && !current_user.employer)
+      @experience.destroy
+      flash[:success] = "Skill Removed."
+    else
+      flash[:error] = "No access"
+    end
+    redirect_to '/experiences'
 
   end
 
@@ -16,14 +52,14 @@ class ExperiencesController < ApplicationController
   		@experience.user_id = current_user.user_id
   		if(@experience.save)
   			flash[:success] = "Experience created!"
-        	redirect_to root_path
+        	redirect_to '/experiences'
         else
   			flash[:error] = "Fill in all required fields"
-        	redirect_to root_path
+        	redirect_to '/experiences'
     end
   	else
   		flash[:error] = "No access"
-        redirect_to root_path
+        redirect_to '/'
 
   	end
 
@@ -34,6 +70,6 @@ class ExperiencesController < ApplicationController
     
     def experience_params
         params.require(:experience).permit(:title, :desc, :experience_id, :sdate, :fdate, :company_name,
-        :company_address, :company_address, :company_city, :company_country, :supervisor, :supervisor_phone)
+        :company_province, :company_address, :company_city, :company_country, :supervisor, :supervisor_phone)
     end
 end
--- spec/controllers/experiences_controller_spec.rb
+++ spec/controllers/experiences_controller_spec.rb
@@ -4,13 +4,7 @@ describe ExperiencesController do
 
   let(:employee) { create(:user, :employee) }
   let(:employer) { create(:user, :employer) }
-  
-  describe "GET #index" do
-    it "renders index experience page" do
-      get :index
-      response.should render_template :index
-    end
-  end
+  let(:test)     { create(:experience) }
   describe "user not signed in" do
     it "should not be able to go to create" do
       get 'create'
@@ -25,13 +19,13 @@ describe ExperiencesController do
       it "should be able to submit a experience through create" do
         post 'create', :experience => { "title" => "title", "desc" => "content", 
           "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test" }
-        response.should redirect_to '/'
+        response.should redirect_to '/experiences'
         flash[:success].should_not be_nil
       end
       it "should not have success flash if unable to save" do
-        post 'create', :experience => { "" => "title", "desc" => "content", 
+        post 'create', :experience => { "title" => "", "desc" => "content", 
           "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test" }
-        response.should redirect_to '/'
+        response.should redirect_to '/experiences'
         flash[:error].should_not be_nil
       end
     end
@@ -41,14 +35,76 @@ describe ExperiencesController do
       end
       it "should not be able to submit a experience through create" do
         post 'create', :experience => { "title" => "title", "desc" => "content", 
-          "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test"  }
+          "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test" }
         response.should redirect_to '/'
         flash[:error].should == "No access"
       end
       it "should have error flash if unable to save" do
         post 'create', :experience => { "title" => "", "desc" => "content", 
-          "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test"  }
+          "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test" }
         response.should redirect_to '/'
+        flash[:error].should_not be_nil
+      end
+    end
+  end
+  describe "#destroy" do
+    describe "employee signed in" do
+      before do
+        sign_in employee
+        test.save
+      end
+      it "delete a experience through destroy" do
+        post 'destroy', :id => 1
+        response.should redirect_to '/experiences'
+        flash[:success].should_not be_nil
+      end
+    end
+    describe "employer signed in" do
+      before do
+        sign_in employer
+        test.save
+      end
+      it "should not be able to delete a experience" do
+        post 'destroy', :id => 1
+        response.should redirect_to '/experiences'
+        flash[:error].should == "No access"
+      end
+    end
+  end
+  describe "#update" do
+    describe "employee signed in" do
+      before do
+        sign_in employee
+        test.save
+      end
+      it "should be able to update a experience through edit" do
+        post 'update', :experience => { "title" => "title", "desc" => "content", 
+          "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test" }, :id => 1
+        response.should redirect_to '/experiences'
+        flash[:success].should_not be_nil
+      end
+      it "should not have success flash if unable to update" do
+        post 'update', :experience => { "title" => "", "desc" => "content", 
+          "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test" }, :id => 1
+        response.should redirect_to '/experiences'
+        flash[:error].should_not be_nil
+      end
+    end
+    describe "employer signed in" do
+      before do
+        sign_in employer
+        test.save
+      end
+      it "should not be able to update a experience through edit" do
+        post 'update', :experience => { "title" => "title", "desc" => "content", 
+          "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test" }, :id => 1
+        response.should redirect_to '/experiences'
+        flash[:error].should == "No access"
+      end
+      it "should have error flash if unable to save" do
+        post 'update', :experience => { "title" => "", "desc" => "content", 
+          "experience_id" => 0, "sdate" => DateTime.now, "company_name" => "company test" }, :id => 1
+        response.should redirect_to '/experiences'
         flash[:error].should_not be_nil
       end
     end
