666638125b2bd5abd45ff1578c7c5ce24d64e4a2
Duncan Szarmes <szarmes@gmail.com>
2013-11-12 17:56:35 -0800

    projects controller tested

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -4,7 +4,8 @@ describe ProjectsController do
 
   let(:employee) { create(:user, :employee) }
   let(:employer) { create(:user, :employer) }
-  let(:experience) { create(:experience) }
+  let(:test) { create(:project) }
+  let(:test1) { create(:experience) }
   
   describe "GET #index" do
     it "renders index projects page" do
@@ -53,5 +54,71 @@ describe ProjectsController do
         flash[:error].should_not be_nil
       end
     end
+ describe "#destroy" do
+    describe "employee signed in" do
+      before do
+        sign_in employee
+        test.save
+        test1.save
+      end
+      it "delete a project through destroy" do
+        post 'destroy', :id => 1
+        response.should redirect_to '/projects'
+        flash[:success].should_not be_nil
+      end
+    end
+    describe "employer signed in" do
+      before do
+        sign_in employer
+        test.save
+        test1.save
+      end
+      it "should not be able to delete a experience" do
+        post 'destroy', :id => 1
+        response.should redirect_to '/projects'
+        flash[:error].should == "No access"
+      end
+    end
   end
+  describe "#update" do
+    describe "employee signed in" do
+      before do
+        sign_in employee
+        test.save
+        test1.save
+      end
+      it "should be able to update a project through edit" do
+       post 'update', :project => { "name" => "title", "desc" => "content", 
+          "experience_id" => 1, "project_id" => 1 }, :id => 1
+        response.should redirect_to '/projects'
+        flash[:success].should_not be_nil
+      end
+      it "should not have success flash if unable to update" do
+        post 'update', :project => { "name" => "", "desc" => "content", 
+          "experience_id" => 1, "project_id" => 0 }, :id => 1
+        response.should redirect_to '/projects'
+        flash[:error].should_not be_nil
+      end
+    end
+    describe "employer signed in" do
+      before do
+        sign_in employer
+        test.save
+        test1.save
+      end
+      it "should not be able to update a project through edit" do
+         post 'update', :project => { "name" => "title", "desc" => "content", 
+          "experience_id" => 1, "project_id" => 1 }, :id => 1
+        response.should redirect_to '/projects'
+        flash[:error].should == "No access"
+      end
+      it "should have error flash if unable to save" do
+        post 'update', :project => { "name" => "", "desc" => "content", 
+          "experience_id" => 1, "project_id" => 1 }, :id => 1
+        response.should redirect_to '/projects'
+        flash[:error].should_not be_nil
+      end
+    end
+  end
+end
 end
