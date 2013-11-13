4236c0fa0742f802cee1596ba5a126271125a95f
Matthew Bojey <mbojey@gmail.com>
2013-11-12 18:27:21 -0800

    References view and controller working and tested

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -3,6 +3,8 @@ class ReferencesController < ApplicationController
   def index
   	@reference = Reference.new
     @user = current_user
+    @userID = @user.user_id
+    @references = Reference.where(:user_id => current_user.user_id).paginate(page: params[:page])
   end
 
   def show
@@ -26,8 +28,39 @@ class ReferencesController < ApplicationController
         redirect_to root_path
 
   	end
+  end
+  def update
+    @reference = Reference.find(params[:id])
+    if (user_signed_in? && current_user.user_id == @reference.user_id && !current_user.employer)
+      if @reference.update_attributes(reference_params)
+        @reference.save
+        flash[:success] = "Changes saved"
+        redirect_to '/references'         
+      else
+        flash[:error] = "Changes not saved."
+        redirect_to '/references'                       
+      end
+    else
+      flash[:error] = "No access"
+      redirect_to '/references'  
+    end
+  end
 
-  	
+  def edit
+    @reference = Reference.find(params[:id])
+    @user = current_user
+  end
+
+  def destroy
+    @reference = Reference.find(params[:id])
+    if (user_signed_in? && current_user.user_id == @reference.user_id && !current_user.employer)
+      @reference.destroy
+      flash[:success] = "Reference Removed."
+    else
+      flash[:error] = "No access"
+    end
+    redirect_to '/references'
+
   end
 
   private
