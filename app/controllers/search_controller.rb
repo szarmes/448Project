53b7cde0b8989cbf9100081daa29a78df510eaa5
Duncan Szarmes <szarmes@gmail.com>
2013-11-19 18:29:35 -0800

    added unique results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   def search
     @user=current_user
     @results = Posting.titlesearch(params[:q]) + Posting.descsearch(params[:q])
+    @results = @results.uniq
     @q = params[:q]
   end
 
